a30ceaf0200ab021412869af4c06b99826192a99
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-05-02 13:08:46 -0300

    C2 - instalado e configurado o Tailwindcss e add um exemplo de como usa-lo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import { useState } from 'react'
+interface ButtonProps {
+  text?: string
+}
+
+function Button(props: ButtonProps) {
+  return <button className="bg-violet-500 px-4 h-10 rounded text-violet-100 hover:bg-violet-700 transition-colors " /*Poderia declarar a cor assim tbm (bg-[#8257e6])*/ /*className="button" --> daria pra fazer asiim tbm, usando o arquivo global*/ >{props.text ?? 'Default'}</button>
+}
 
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="flex gap-2">
+      <Button text="Enviar"/>
+      <Button text="Ok"/>
+      <Button />
+    </div>
+  )
 }
 
 export default App
