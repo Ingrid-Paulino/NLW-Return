4ed51a1a411aba9f856f12014ca2c5d9fb15ec4c
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-05-02 11:47:51 -0300

    C1 - limpesa na aplicaçao + Hello Word

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+  // Arquivo de configuração que indica que ele esta usando o plugin do react.
 export default defineConfig({
   plugins: [react()]
 })
