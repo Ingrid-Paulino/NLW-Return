38e320411edb5b46f47f789fc76d533eb8f58838
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-05-02 15:31:46 -0300

    C4 - add acessibilidade com Popover

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,29 +1,45 @@
 import { useState } from 'react';
 import { ChatTeardropDots } from 'phosphor-react'
+import { Popover } from '@headlessui/react'
 
 export function Widget() {
-  const [isWidgetOpen, setIsWidgetOpen] =  useState(false)
+  // Forma 1 sem usar Popover
+  // const [isWidgetOpen, setIsWidgetOpen] =  useState(false)
 
-  const toggleWidgetVisibility = () => {
-    setIsWidgetOpen(!isWidgetOpen)
-  }
+  // const toggleWidgetVisibility = () => {
+  //   setIsWidgetOpen(!isWidgetOpen)
+  // }
 
   return (
-    <div className='absolute bottom-5 right-5'>
+    <Popover className='absolute bottom-5 right-5'>
+    {/* <div className='absolute bottom-5 right-5'> troquei a div por Popover */}
+      {/* FORMA 1 */}
+      {/* OBS: usando o Popover não vou precisar dessa linha 18 */}
       {/* {isWidgetOpen ? <p>Hellow world</p> : null} */}
       {/* OU */}
+      {/* {isWidgetOpen && <p>Hellow world</p>} São equivaletes -> so executa a segunda parte se a primeira for verdadeiro */}
+   
+      {/* FORMA 2 */}
+      <Popover.Panel>Hello World</Popover.Panel>
+    
       
-      {isWidgetOpen && <p>Hellow world</p>} {/* São equivaletes -> so executa a segunda parte se a primeira for verdadeiro */}
 
-      
-      <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+
+    {/* forma 2 */}
+       <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group"> 
+
+    
+      {/* forma 1 */}
+      {/* <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group"> */}
         <ChatTeardropDots className='w-6 h-6'  /*size="200" weight="thin" weight="bold" weight="fill" weight="duotone"*/ />
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear"> 
         {/* overflow-hidden --> esconte qualquer texto ou conteudo de alguma div ou elemento que sobreponha o tamanho maximo daquele elemrnto fica invisivel  */}
           <span className="pl-2"></span>
           FeedBack
         </span>
-      </button>
-    </div>
+      </Popover.Button>
+    {/* </div> */}
+    </Popover>
+
   ) 
 }
